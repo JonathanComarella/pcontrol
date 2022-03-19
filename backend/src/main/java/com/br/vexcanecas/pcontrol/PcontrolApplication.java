26a1f6dd4c619410246b77d3af92e21fec6181a9
Jonathan Comarella <jonathanwcomarella@gmail.com>
2022-03-18 21:18:54 -0300

    Projeto iniciado

---
+++ backend/src/main/java/com/br/vexcanecas/pcontrol/PcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.br.vexcanecas.pcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcontrolApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/br/vexcanecas/pcontrol/PcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.vexcanecas.pcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcontrolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
